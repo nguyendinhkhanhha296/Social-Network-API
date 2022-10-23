6cc2ee91ae4845e1acbf3f2f8f03b4ea9eb52e58
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-10-23 01:10:22 -0700

    seed data into app

---
+++ utils/data.js
@@ -0,0 +1,60 @@
+const seedUser = [{
+        username: "Ha Nguyen",
+        email: "hanguyen@gmail.com",
+    },
+    {
+        username: "David Wang",
+        email: "dw123@yahoo.com",
+    },
+    {
+        username: "Lotte Ria",
+        email: "lotteee_ria@cox.net",
+    },
+    {
+        username: "Kfc Vietnam",
+        email: "kfc.vn@icloud.com",
+    },
+];
+
+const seedThought = [{
+        thoughtText: "Hello everyone!",
+        username: "Ha Nguyen",
+    },
+    {
+        thoughtText: "Let's go to eat fried chicken!",
+        username: "David Wang",
+    },
+    {
+        thoughtText: "Come to my store, please.",
+        username: "Lotte Ria",
+    },
+    {
+        thoughtText: "No, my store is better choice.",
+        username: "Kfc Vietnam",
+    },
+    {
+        thoughtText: "Mine, mine, mine.",
+        username: "Lotte Ria",
+    },
+    {
+        thoughtText: "Our drums are really juicy.",
+        username: "Kfc Vietnam",
+    },
+    {
+        thoughtText: "I'll go for KFC. David may choose Lotteria",
+        username: "Ha Nguyen",
+    },
+    {
+        thoughtText: "Hey! I am going to Popeyes. It has good deals.",
+        username: "David Wang",
+    },
+    {
+        thoughtText: "Ohhhh, noooooo",
+        username: "Lotte Ria",
+    }
+];
+
+module.exports = {
+    seedUser,
+    seedThought
+};
