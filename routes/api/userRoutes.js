22033ec8332b41368b8018de222c4d869a98abaf
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-10-22 23:53:27 -0700

    function api router for User and update script for package.json

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// getting all users / creating a user
+router.route("/").get(getUsers).post(createUser);
+
+// get a single user / delete a user / update a user by id
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// Add a friend to user
+router.route("/:userId/friends/:friendId").post(addFriend);
+
+// Delete a friend from user
+router.route("/:userId/friends/:friendId").delete(deleteFriend);
+
+module.exports = router;
