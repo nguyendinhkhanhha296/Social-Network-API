4b24d42d210e134c5b53f46a4ff5fa81c4bd05a1
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-10-23 00:45:23 -0700

    change old mongoose to the updated one

--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,6 @@
 const { Schema, Types, model } = require("mongoose");
 const moment = require("moment");
 
-// thought Schema
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      maxlength: 280,
-      minlength: 1,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) =>
-        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
-
 //reaction Schema
 const reactionSchema = new Schema(
     {
@@ -58,6 +30,36 @@ const reactionSchema = new Schema(
         getters: true,
       },
       id: false,
+    }
+  );
+
+module.exports = reactionSchema;
+
+// thought Schema
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) =>
+          moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
     }
   );
   
